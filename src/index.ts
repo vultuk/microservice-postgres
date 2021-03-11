5f19fc30d1bf241aec72c1c04224acc15750ca3d
Simon Skinner <vultuk28@gmail.com>
2021-03-11 17:12:09 +0000

    Postgres additions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-// Start the package here
+import {Request} from 'express';
+import {Pool} from 'pg';
+
+import {Settings} from './Types/Settings';
+
+export * from './Types';
+declare global {
+  namespace Express {
+    interface Request {
+      postgres: Pool;
+    }
+  }
+}
+
+export default (settings: Settings) => (req: Request) => {
+  const db = new Pool({ connectionString: settings.connectionString });
+
+  req.postgres = db;
+};
