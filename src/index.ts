0e6ab01e92b81b2e0e2d587770dbdb2f632bdae5
Simon Skinner <vultuk28@gmail.com>
2021-03-11 17:27:07 +0000

    Closing pool on sigterm

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request, Response } from 'express';
-import { Pool } from 'pg';
+import {NextFunction, Request, Response} from 'express';
+import {Pool} from 'pg';
 
-import { Settings } from './Types/Settings';
+import {Settings} from './Types/Settings';
 
 export * from './Types';
 declare global {
@@ -16,6 +16,13 @@ export default (settings: Settings) => (req: Request, res: Response, next: NextF
   const db = new Pool({ connectionString: settings.connectionString });
 
   req.postgres = db;
+
+  // When we get a sigterm, lets close our database pool
+  process.on('SIGTERM', async () => {
+    await db.end();
+    console.log('Database Disconnected - Happy to shutdown');
+    process.exit(0);
+  });
 
   next();
 };
